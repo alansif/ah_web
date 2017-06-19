670fc280a1604b8ec251bc9d417db7947b0f8bb6
alansif <alansif@sina.com>
2017-06-19 17:16:06 +0800

    bkfinal

---
+++ src/components/bkfinal.vue
@@ -0,0 +1,51 @@
+﻿<template>
+    <div>
+        <div style="width:100%;height:260px;overflow:hidden;">
+            <img src="../assets/sch.jpg" style="width:100%;display:block;">
+            <div class="bftitle">
+                预约
+            </div>
+        </div>
+        <div class="bfframe">
+            <div style="text-align:center;color:#00df00;font-size:1.5em;margin-top:20px;">
+              <i class="el-icon-check"></i>您的预约已成功，谢谢！
+            </div>
+            <div style="position:absolute;top:210px;left:0;text-align:center;width:100%;">
+              <a href="/"><el-button type="primary">返回首页</el-button></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style>
+    .bftitle {
+        position: relative;
+        height: 50px;
+        top: -98px;
+        background-color: rgba(100, 149, 237, 0.5);
+        font-size: 1.3em;
+        color: white;
+        padding-left: 20px;
+        padding-top: 10px;
+    }
+
+    .bfframe {
+        position: relative;
+        height: 255px;
+        border: 1px solid cornflowerblue;
+        border-radius: 2px;
+        padding: 24px;
+        background-color: #f7f7f7;
+    }
+</style>
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import report from './components/report.vue'
 import bkquery from './components/bkquery.vue'
 import bkstep1 from './components/bkstep1.vue'
 import bkstep2 from './components/bkstep2.vue'
+import bkfinal from './components/bkfinal.vue'
 
 export default [
     { path: '/', component: mainpage },
@@ -11,5 +12,6 @@ export default [
     { path: '/report', component: report },
     { path: '/bkquery', component: bkquery },
     { path: '/bkstep1', component: bkstep1 },
-    { path: '/bkstep2', component: bkstep2 }
+    { path: '/bkstep2', component: bkstep2 },
+    { path: '/bkfinal', component: bkfinal }
 ];
