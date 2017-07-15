32d4458738aa42175a20ea6e11b35daecaff29b7
alansif <alansif@sina.com>
2017-07-15 22:08:36 +0800

    bulletin

--- src/components/bltpage.vue
+++ src/components/bltpage.vue
@@ -1,88 +1,132 @@
 ﻿<template>
-	<div>
-      <div style="width:100%;height:260px;overflow:hidden;">
-        <img src="../assets/physician.jpg" style="width:100%;display:block;">
-        <div class="blttitle">
-          新闻&公告
+    <div>
+        <div style="width:100%;height:260px;overflow:hidden;">
+            <img src="../assets/physician.jpg" style="width:100%;display:block;">
+            <div class="blttitle">
+                新闻&公告
+            </div>
         </div>
-      </div>
-    <el-tabs v-model="activeName2" @tab-click="handleClick" class="blttab">
-      <el-tab-pane label="新闻公告" name="news">
-        <el-table :data="newsData" :height="600" :show-header="false" class="blttable">
-          <el-table-column
-            prop="title"
-            >
-          </el-table-column>
-          <el-table-column
-            prop="date"
-            width="200">
-          </el-table-column>
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane label="勘误声明" name="errata">
-        <el-table :data="errataData" :height="600" :show-header="false" class="blttable">
-          <el-table-column
-            prop="title"
-            >
-          </el-table-column>
-          <el-table-column
-            prop="date"
-            width="200">
-          </el-table-column>
-        </el-table>
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+        <el-tabs v-model="activeName2" class="blttab">
+            <el-tab-pane label="新闻公告" name="news">
+                <el-table :data="newsData" :height="600" :show-header="false" class="blttable" @row-click="handlerow">
+                    <el-table-column
+                            prop="title"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                            prop="issuedate"
+                            width="200">
+                    </el-table-column>
+                </el-table>
+            </el-tab-pane>
+            <el-tab-pane label="勘误声明" name="errata">
+                <el-table :data="errataData" :height="600" :show-header="false" class="blttable" @row-click="handlerow">
+                    <el-table-column
+                            prop="title"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                            prop="issuedate"
+                            width="200">
+                    </el-table-column>
+                </el-table>
+            </el-tab-pane>
+        </el-tabs>
+        <el-dialog :visible.sync="dialogVisible">
+            <p style="text-align: center;font-weight: bold;font-size:18px;">{{btitle}}</p>
+            <p style="text-align: center">{{bdate}}</p>
+            <p v-html="bcontent" style="line-height: 24px;text-align: justify;padding: 8px 32px 0 32px;"></p>
+            <div slot="footer" style="text-align: center">
+                <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-       activeName2: 'news',
-       newsData:[
-         {title:"2017春节放假通知",date:"2016-12-10"},
-         {title:"2017端午节放假通知",date:"2017-5-1"},
-         {title:"2017国庆节放假通知",date:"2017-9-12"}
-         ],
-       errataData:[
-         {title:"勘误声明A",date:"2016-2-10"},
-         {title:"勘误声明B",date:"2017-5-11"}
-         ]
-      };
-    },
-    methods: {
-      handleClick(tab, event) {
-        console.log(tab, event);
-      }
+    import {restbase} from '../config'
+    export default {
+        data() {
+            return {
+                activeName2: 'news',
+                newsData: [],
+                errataData: [],
+                dialogVisible: false,
+                btitle: '',
+                bdate:'',
+                bcontent: ''
+            }
+        },
+        mounted() {
+            this.$http.get(restbase() + "api/v1/bulletin/summary", {
+            }).then((response) => {
+                if (response.body.status.code == 0) {
+                    response.body.data.forEach(v=>{
+                        v.issuedate = moment(v.issuedate).format('YYYY-MM-DD');
+                        if (v.category === '勘误') {
+                            this.errataData.push(v);
+                        } else {
+                            this.newsData.push(v);
+                        }
+                    });
+                } else {
+                    console.log(response);
+                }
+            }, (response) => {
+                console.log(response);
+            }).catch((response) => {
+                console.log(response);
+            });
+        },
+        methods: {
+            handlerow(row) {
+                this.$http.get(restbase() + `api/v1/bulletin/${row.uid}/detail`, {
+                }).then((response) => {
+                    if (response.body.status.code == 0) {
+                        if (response.body.data.length === 1) {
+                            var v = response.body.data[0];
+                            this.btitle = v.title;
+                            this.bdate = moment(v.issuedate).format('YYYY-MM-DD');
+                            this.bcontent = v.body;
+                            this.dialogVisible = true;
+                        }
+                    } else {
+                        console.log(response);
+                    }
+                }, (response) => {
+                    console.log(response);
+                }).catch((response) => {
+                    console.log(response);
+                });
+            }
+        }
     }
-  }
 </script>
 
 <style>
-  .blttab {
-    border: 1px solid cornflowerblue;
-    border-radius: 2px;
-  }
+    .blttab {
+        border: 1px solid cornflowerblue;
+        border-radius: 2px;
+    }
 
-  .blttable {
-    width: 100%;
-    color: #777;
-  }
+    .blttable {
+        width: 100%;
+        color: #777;
+    }
 
-  .el-tabs__header {
-    margin-bottom: 1px;
-    background-color: #f7f7f7;
-  }
+    .el-tabs__header {
+        margin-bottom: 1px;
+        background-color: #f7f7f7;
+    }
 
-  .blttitle {
-    position:relative;
-    height:50px;
-    top:-116px;
-    background-color:rgba(100,149,237,0.5);
-    font-size:1.3em;
-    color:white;
-    padding-left: 20px;
-    padding-top: 10px;
-  }
+    .blttitle {
+        position: relative;
+        height: 50px;
+        top: -116px;
+        background-color: rgba(100, 149, 237, 0.5);
+        font-size: 1.3em;
+        color: white;
+        padding-left: 20px;
+        padding-top: 10px;
+    }
 </style>
--- src/components/bulletin.vue
+++ src/components/bulletin.vue
@@ -3,24 +3,14 @@
             <div style="position: absolute;width: 100%;height:40px;background-color: rgba(100, 149, 237, 0.5);"></div>
             <div class="swiper-container gonggao-box" id="gg">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <a href="/blt">
-                            <img src="../assets/megaphone.svg" class="gonggao-pic" />
-                            <div class="gonggao-text">2017年元旦放假通知</div>
-                        </a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="/blt">
-                            <img src="../assets/megaphone.svg" class="gonggao-pic" />
-                            <div class="gonggao-text">2017年春节放假通知</div>
-                        </a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="/blt">
-                            <img src="../assets/megaphone.svg" class="gonggao-pic" />
-                            <div class="gonggao-text">2017年中秋节放假通知</div>
-                        </a>
-                    </div>
+                    <template v-for="item in rows">
+                        <div class="swiper-slide">
+                            <a href="/blt">
+                                <img src="../assets/megaphone.svg" class="gonggao-pic" />
+                                <div class="gonggao-text">{{item.title}}</div>
+                            </a>
+                        </div>
+                    </template>
                 </div>
             </div>
             <div class="ggbtn">
@@ -32,21 +22,44 @@
 </template>
 
 <script>
+    import {restbase} from '../config'
+
+    function initSwiper() {
+        var mySwiper1 = new Swiper('.gonggao-box', {
+            autoplayDisableOnInteraction: false,
+            direction: 'vertical',
+            loop: true,
+            autoplay: 3000,//可选选项，自动滑动
+            onlyExternal: true
+        });
+        var gg = document.getElementById("gg");
+        gg.addEventListener("mouseenter", function () {
+            mySwiper1.stopAutoplay();
+        });
+        gg.addEventListener("mouseleave", function () {
+            mySwiper1.startAutoplay();
+        });
+    }
+
     export default {
+        data() {
+            return {
+                rows:[]
+            }
+        },
         mounted() {
-            var mySwiper1 = new Swiper('.gonggao-box', {
-                autoplayDisableOnInteraction: false,
-                direction: 'vertical',
-                loop: true,
-                autoplay: 3000,//可选选项，自动滑动
-                onlyExternal: true
-            });
-            var gg = document.getElementById("gg");
-            gg.addEventListener("mouseenter", function () {
-                mySwiper1.stopAutoplay();
-            });
-            gg.addEventListener("mouseleave", function () {
-                mySwiper1.startAutoplay();
+            this.$http.get(restbase() + "api/v1/bulletin/carousel", {
+            }).then((response) => {
+                if (response.body.status.code == 0) {
+                    this.rows = response.body.data;
+                    this.$nextTick(initSwiper);
+                } else {
+                    console.log(response);
+                }
+            }, (response) => {
+                console.log(response);
+            }).catch((response) => {
+                console.log(response);
             });
         }
     }
